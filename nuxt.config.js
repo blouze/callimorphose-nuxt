12adb668223851df56a0d1087b024c8275023c1c
Blaise <blouze@gmail.com>
2020-09-13 12:41:15 +0200

    baseURL = URL || '/'

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,6 +20,7 @@ export default {
     ]
   },
   env: {
+    baseURL: process.env.URL || '/',
     siteName: 'Callimorphose, atelier de calligraphie',
     siteAuthor: 'DISPLAY JERKY',
     emailAdress: process.env.EMAIL_ADRESS || 'bonjour@callimorphose.com',
